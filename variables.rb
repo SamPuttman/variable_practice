8ba3dbf164eca7f20f07b196fd569b31565cb3a5
Sam Puttman <daputter2@gmail.com>
2023-08-28 18:58:39 -0500

    add titles and cleanup

--- variables.rb
+++ variables.rb
@@ -1,24 +1,36 @@
 ##Variables Practice
 
+#Variables assigned to Strings
+
 student_name = "Sam Puttman"
 student_city = "Saint Louis"
 favorite_game = "Baldur's Gate 3"
+
+#Variables assigned to Integers
 
 student_age = 30
 favorite_pokemon_number = 007
 address_number = 840
 
+#Variables assigned to Floats
+
 grocery_bill = 77.64
 rent_bill = 1234.56
 phone_bill = 124.82
+
+#Variables assigned to Booleans
 
 enjoying_mod0 = true
 feeling_good = true
 likes_ranch = false
 
+#Reassigned Strings
+
 student_name = "Ronald McDonald"
 student_city = "McDonaldland"
 favorite_game = "Life"
+
+#puts
 
 puts student_name
 puts student_city
